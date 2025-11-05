7a8b61d7ed6dc83f82f8f3382fb4b7b6940a78b2
jo-hseo <ellejo1110@gmail.com>
2025-11-05 23:51:36 +0900

    add(gateway): 예외처리 추가

---
+++ src/main/java/com/sparta/gatewayservice/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.sparta.gatewayservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    GATEWAY_ERROR(11000, HttpStatus.INTERNAL_SERVER_ERROR, "게이트웨이 에러 발생"),
+    GATEWAY_JSON_PROCESSING_EXCEPTION(11001, HttpStatus.INTERNAL_SERVER_ERROR, "게이트웨이에서 에러처리하는 중에 객체를 json으로 변환하지 못했습니다"),
+    ;
+
+    private final int code;
+    private final HttpStatus status;
+    private final String details;
+}
